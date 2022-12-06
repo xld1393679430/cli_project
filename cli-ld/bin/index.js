cf338be2892eccd3b110a315377d951482b35b14
ld_xu <ld_xu@trip.com>
2022-12-06 16:52:55 +0800

    使用 yargs 搭建脚手架

--- cli-ld/bin/index.js
+++ cli-ld/bin/index.js
@@ -1,35 +1,49 @@
 #! /usr/bin/env node
 
-const argv = require("process").argv;
-const lib = require("cli-ld-lib");
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const dedent = require("dedent");
 
-const command = argv[2];
-
-const options = argv.slice(3);
-
-console.log("----argv----", command, argv);
-
-
-if (options.length > 1) {
-  let [option, param] = options;
-  option = option?.replace(/--/, "");
-
-  if (command) {
-    if (lib[command]) {
-      lib[command]({ option, param });
-    } else {
-      console.log("无效的命令");
-    }
-  } else {
-    console.log("请输入命令");
-  }
-}
-
-// 实现参数解析 --version 或者 -v
-if (command.startsWith('--') || command.startsWith('-')) {
-	const globalOption = command.replace(/--|-/g, '')
-	console.log("---globalOption---", globalOption)
-	if (['version', 'v', 'V'].includes(globalOption)) {
-		console.log("1.0.0")
-	}
-}
+const arg = hideBin(process.argv);
+const cli = yargs(arg);
+cli
+  .usage("Usage: cli-ld [command] <options>")
+  .demandCommand(1, "请最少输入一个参数. Press --help to see all available commands and options")
+  .strict()
+  .recommendCommands()
+  .fail((err, msg) => {
+	console.error("err:", err)
+	console.info("msg:", msg)
+  })
+  .alias("h", "help")
+  .alias("v", "version")
+  .alias("V", "version")
+  .wrap(cli.terminalWidth() / 2) // 命令行宽度
+  .epilogue(
+    dedent`
+		欢迎使用
+	`
+  ) //页脚添加的字符
+  .options({
+    debug: {
+      type: "boolean",
+      description: "启动调试模式",
+      alias: "d",
+    },
+  })
+  .option("registry", {
+    type: "string",
+    description: "定义全局仓库地址",
+    alias: "r",
+  })
+  .group(["debug"], "开发选项:")
+  .command("init [name]", "项目初始化", (yargs) => {
+    yargs.option("name", {
+      type: "string",
+      describe: "项目的名称",
+      alias: "n",
+    }),
+      (argv) => {
+        console.log(argv);
+      };
+  }).argv;
