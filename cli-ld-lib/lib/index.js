5e758fa3f49f9bcd5e419e214cbb2f2732313d90
ld_xu <ld_xu@trip.com>
2022-12-06 15:23:13 +0800

    Initial commit

---
+++ cli-ld-lib/lib/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+	sum(a, b) {
+		return a + b;
+	},
+	delete(a, b) {
+		return a - b;
+	}
+}
---
+++ cli-ld/bin/index.js
@@ -0,0 +1,8 @@
+#! /usr/bin/env node
+
+const { sum } = require("cli-ld-lib")
+
+const total = sum(1, 2)
+console.log(sum, total, '2222')
+
+console.log("hello!!!223");
