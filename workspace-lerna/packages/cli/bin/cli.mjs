92d05e2fb0566f892d53f8fed35545f9f335305e
ld_xu <ld_xu@trip.com>
2022-12-14 17:41:24 +0800

    add useless

--- workspace-lerna/packages/cli/bin/cli.mjs
+++ workspace-lerna/packages/cli/bin/cli.mjs
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
 'use strict';
 
-import cli from '../src/cli.mjs'
 import logA from '@xld-cli-dev/test-lerna-a'
 import logB from '@xld-cli-dev/test-lerna-b'
-
-cli().parse(process.argv.slice(2));
 
 logA();
 
